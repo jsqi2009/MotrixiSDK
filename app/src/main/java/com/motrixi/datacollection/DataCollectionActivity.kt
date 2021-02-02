239fd90cabe5ff5916e93a58556f64d6bb9e7966
jsqi <qijs@shinetechsoftware.com>
2021-02-02 14:49:22 +0800

    adjust the logic to adapt the language list

--- app/src/main/java/com/motrixi/datacollection/DataCollectionActivity.kt
+++ app/src/main/java/com/motrixi/datacollection/DataCollectionActivity.kt
@@ -458,14 +458,14 @@ class DataCollectionActivity : AppCompatActivity() {
                             val item:JSONObject = resultArray.get(i) as JSONObject
                             val valueObj:JSONObject = item.optJSONObject("value")
                             val languageObj:JSONObject = valueObj.optJSONObject("select_language")
-                            val code = languageObj.optString("code")
-                            val language = languageObj.optString("language")
 
-//                            var info: LanguageInfo = LanguageInfo()
-//                            info.code = code
-//                            info.language = language
-                            lanList.add(LanguageInfo(code, language))
-                            Log.e("language info", "$code-$language")
+                            if (languageObj.has("code") && languageObj.has("language")) {
+                                val code = languageObj.optString("code")
+                                val language = languageObj.optString("language")
+                                lanList.add(LanguageInfo(code, language))
+                                Log.e("language info", "$code-$language")
+                            }
+
                         }
                     } else {
                         UploadLogUtil.uploadLogData(context, "get consent list failure ")
